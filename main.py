9db2c8c1477607740478419612c78907c488fcf1
vinnowin <vincenthuf@live.nl>
2024-09-13 15:31:22 +0200

    beetje preprocess

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 class Node:
     def __init__(self, feature=None, threshold=None, left=None, right=None, value=None, gain=0):
         self.feature = feature      # Index of the feature to split on
@@ -8,9 +11,18 @@ class Node:
         self.gain = gain
 
 class Tree:
-    def __init__():
+    def __init__(self):
         pass
 
-    
+def pre_process(data):
+    data = pd.DataFrame(data, columns = ['age','married','house','income','gender','class'])
+    data = data.astype(int)
+    classification = data['class']
+    features = data.drop('class', axis=1)
+    return features, classification
+
+
+
 if __name__ == "__main__":
-    pass
+    data = np.genfromtxt('credit.txt', delimiter=',', skip_header=True)
+    x, y = pre_process(data)
